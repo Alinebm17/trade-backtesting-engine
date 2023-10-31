ae7a66214959890609a1bd9a1213e8b2dd25fb52
Maksym Shamko <max.shamko25@gmail.com>
2023-10-31 13:53:57 +0200

    log

--- src/dca/index.ts
+++ src/dca/index.ts
@@ -150,7 +150,6 @@ class DCABacktesting extends Backtesting {
           result.duration.firstDataTime = this.period.from * 1000
           result.duration.lastDataTime = this.period.to * 1000
         }
-        console.log(result)
         return result
       }
     })
