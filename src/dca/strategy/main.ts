b2364c03b604bbe248939dff0e18f61890a69546
Maksym Shamko <max.shamko25@gmail.com>
2023-11-20 16:12:07 +0200

    % in ratios

--- src/dca/strategy/main.ts
+++ src/dca/strategy/main.ts
@@ -3762,7 +3762,7 @@ export abstract class Strategy implements StrategyInterface {
           (d) => d.closedTime && d.closedTime >= prev && d.closedTime < i,
         )
 
-        const profit = deals.reduce((acc, v) => (acc += v.profit.total), 0)
+        const profit = deals.reduce((acc, v) => (acc += v.profit.perc / 100), 0)
         profitByPeriod.push(profit)
         if (periodRatio === 365) {
           startDate.setHours(24)
