527fccbd086039b9399b24c044a20398b213f214
Maksym Shamko <max.shamko25@gmail.com>
2025-04-29 12:37:42 +0300

    default kc range length

--- src/dca/strategy/ti/index.ts
+++ src/dca/strategy/ti/index.ts
@@ -255,7 +255,7 @@ class TIStrategy extends Strategy implements StrategyInterface {
                 ma: kcMa || MAEnum.ema,
                 multiplier: bbwMult || 2,
                 range: kcRange || RangeType.atr,
-                rangeLength: kcRangeLength || 10,
+                rangeLength: kcRangeLength || 20,
               }
             : type === IndicatorEnum.kcpb
             ? {
@@ -264,7 +264,7 @@ class TIStrategy extends Strategy implements StrategyInterface {
                 ma: kcMa || MAEnum.ema,
                 multiplier: bbwMult || 2,
                 range: kcRange || RangeType.atr,
-                rangeLength: kcRangeLength || 10,
+                rangeLength: kcRangeLength || 20,
                 percentile,
                 percentileLookback,
                 percentilePercentage,
