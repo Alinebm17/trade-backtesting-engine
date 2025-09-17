f62461f053c21870169b8d80bb583e35e667c34d
Maksym Shamko <max.shamko25@gmail.com>
2025-09-17 14:39:52 +0300

    wip: hyperliquid integration

--- src/types.ts
+++ src/types.ts
@@ -1045,6 +1045,12 @@ export enum ExchangeEnum {
   paperBitgetSpot = 'paperBitgetSpot',
   mexc = 'mexc',
   paperMexc = 'paperMexc',
+  hyperliquid = 'hyperliquid',
+  hyperliquidInverse = 'hyperliquidInverse',
+  paperHyperliquid = 'paperHyperliquid',
+  paperHyperliquidInverse = 'paperHyperliquidInverse',
+  hyperliquidAll = 'hyperliquidAll',
+  paperHyperliquidAll = 'paperHyperliquidAll',
 }
 
 export type DCAGrid = {
